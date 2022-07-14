5d4fc0a3b040feeb4baadb6309ff545ad7c69421
Алексей <itale@mail.ru>
2022-07-14 20:47:18 +0500

    hmr

--- bin/dev.js
+++ bin/dev.js
@@ -1,9 +1,30 @@
 const webpack = require('webpack');
-const webpackConfig = require('../webpack.config');
+const [webpackClientConfig, webpackServerConfig] = require('../webpack.config');
 const nodemon = require('nodemon');
 const path = require('path');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+const express = require('express');
 
-const compiler = webpack(webpackConfig);
+const hmrServer = express();
+const clientCompiler = webpack(webpackClientConfig);
+
+hmrServer.use(webpackDevMiddleware(clientCompiler, {
+    publicPath: webpackClientConfig.output.publicPath,
+    serverSideRender: true,
+    writeToDisk: true,
+    stats: 'errors-only',
+}));
+
+hmrServer.use(webpackHotMiddleware(clientCompiler, {
+    path: '/static/__webpack_hmr',
+}));
+
+hmrServer.listen(3001, () => {
+    console.log('HMR server is success')
+});
+
+const compiler = webpack(webpackServerConfig);
 
 compiler.run((err) => {
     if (err) {
@@ -24,4 +45,4 @@ compiler.run((err) => {
             path.resolve(__dirname, '../dist/client'),
         ]
     })
-});
+}); 
